221d1776ab4611c04427711e2d8aaf5dee96d75e
xmarcjordanx <mhujanen1@gmail.com>
2024-04-21 22:23:29 -0600

    Updated Movies.js

--- Movies.js
+++ Movies.js
@@ -1,11 +1,20 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-mongoose.connect(process.env.DB);
+mongoose.connect(process.env.DB, {useNewUrlParser: true, useUnifiedTopology: true });
 
 // Movie schema
 var MovieSchema = new Schema({
-
+    title: { type: String, required: true, index: true },
+    releaseDate: { type: Number, min: [1900, 'Must be greater than 1899'], max: [2100, 'Must be less than 2100']},
+    genre: {
+      type: String,
+      enum: ['Action', 'Adventure', 'Comedy', 'Drama', 'Fantasy', 'Horror', 'Mystery', 'Thriller', 'Western', 'Science Fiction'],
+    },
+    actors: [{
+      actorName: String,
+      characterName: String,
+    }],
 });
 
 // return the model
