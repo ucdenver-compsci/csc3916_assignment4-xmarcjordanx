d542f6bed565f07e4f62f82abd5eb8205c1a4065
xmarcjordanx <mhujanen1@gmail.com>
2024-04-21 23:48:55 -0600

    Updated Async.js

---
+++ Async.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const Movie = require('./models/Movies');
+
+mongoose.connect('mongodb://localhost:27017/webapi', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+// Define the aggregation pipeline
+Movies.aggregate([
+  {
+    $match: { _id: movieId }
+    $lookup: {
+      from: "reviews",
+      localField: "reviews",
+      foreignField: "_id",
+      as: "movieReviews" 
+    }
+  },
+]).exec(function(err, results) => {
+  if (err) {
+    // Handle error
+    console.error(err);
+  } else {
+    console.log(result);
+  }
+});
