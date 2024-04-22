90658e72a86258f4432772eb8f315ecf42a31124
xmarcjordanx <mhujanen1@gmail.com>
2024-04-21 23:49:13 -0600

    Updated Async.js

--- Async.js
+++ Async.js
@@ -10,14 +10,16 @@ mongoose.connect('mongodb://localhost:27017/webapi', {
 Movies.aggregate([
   {
     $match: { _id: movieId }
+  },
+  {
     $lookup: {
       from: "reviews",
       localField: "reviews",
       foreignField: "_id",
       as: "movieReviews" 
     }
-  },
-]).exec(function(err, results) => {
+  }
+]).exec(function(err, results) {
   if (err) {
     // Handle error
     console.error(err);
